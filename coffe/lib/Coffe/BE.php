6b257d9c4fcced529dd6c5c5bd8636471d7bd2ba
demidov-i-a <demidov.i.a@yandex.ru>
2012-08-31 11:49:24 +0400

    Правка функции вывода иконок для LiveForm

--- coffe/lib/Coffe/BE.php
+++ coffe/lib/Coffe/BE.php
@@ -18,8 +18,24 @@ class Coffe_BE
 	}
 
 
+	/**
+	 * Функция выводит иконки редактирования записей LiveForm
+	 *
+	 * @static
+	 * @param $table
+	 * @param $row
+	 * @param null $back_url
+	 * @param null $key
+	 * @param null $rows
+	 * @param array $buttons
+	 * @return string
+	 */
 	public static function renderEditTableButtons($table, $row, $back_url = null, $key = null, $rows = null, $buttons = array('add','edit','moveup','movedown','moveup'))
 	{
+
+		$add_back_url = (is_array($back_url) && isset($back_url['add'])) ? $back_url['add'] : $back_url;
+		$edit_back_url = (is_array($back_url) && isset($back_url['edit'])) ? $back_url['edit'] : $back_url;
+
 		$GLOBALS['LANG']->loadFile('PATH_COFFE:admin/lang/interface.xml');
 		$live_form = Coffe_ModuleManager::getLiveForm($table);
 		$content = '';
@@ -28,18 +44,27 @@ class Coffe_BE
 			$content .= '<ul>';
 			if (in_array('add', $buttons)){
 				$content .= '<li>';
-				$content .= '<a href="'.self::urlLF($table, null, array('back_url' => $back_url)).'">';
+				$content .= '<a href="'.self::urlLF($table, null, array('back_url' => $add_back_url)).'">';
 				$content .= Coffe_Functions::getIcon('coffe/admin/icons/;common/addr');
 				$content .= '</a>';
 				$content .= '</li>';
 			}
 			if (in_array('edit', $buttons)){
 				$content .= '<li>';
-				$content .= '<a href="'.self::urlLF($table, $row[$live_form['primary']],array('back_url' => $back_url)).'">';
+				$content .= '<a href="'.self::urlLF($table, $row[$live_form['primary']],array('back_url' => $edit_back_url)).'">';
 				$content .= Coffe_Functions::getIcon('coffe/admin/icons/;common/edit');
 				$content .= '</a>';
 				$content .= '</li>';
 			}
+
+			if (in_array('remove', $buttons)){
+				$content .= '<li>';
+				$content .= '<a href="'.self::urlLF($table, $row[$live_form['primary']],array('operation' => 'remove','back_url' => $edit_back_url)).'">';
+				$content .= Coffe_Functions::getIcon('coffe/admin/icons/;common/remove');
+				$content .= '</a>';
+				$content .= '</li>';
+			}
+
 			$content .= '</ul>';
 			$content .= '</div>';
 		}
